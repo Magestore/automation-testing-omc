638402c429c2680d22608f2b32df6cb862d7cdec
bang <bang@trueplus.vn>
2017-11-22 16:34:18 +0700

    store credit menu test

---
+++ dev/tests/functional/tests/app/Magento/Customercredit/Test/Block/CreditProduct/CreditProductGridPageActions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 11/22/2017
+ * Time: 4:14 PM
+ */
+
+namespace Magento\Customercredit\Test\Block\CreditProduct;
+
+use Magento\Backend\Test\Block\GridPageActions;
+
+class CreditProductGridPageActions extends GridPageActions
+{
+}
---
+++ dev/tests/functional/tests/app/Magento/Customercredit/Test/TestCase/CreditProductFormTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 11/22/2017
+ * Time: 3:41 PM
+ */
+
+namespace Magento\Customercredit\Test\TestCase;
+
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Customercredit\Test\Page\Adminhtml\CreditProductIndex;
+
+/**
+ * Class CreditProductFormTest
+ * @package Magento\Customercredit\Test\TestCase
+ */
+class CreditProductFormTest extends Injectable
+{
+    /**
+     * @var CreditProductIndex
+     */
+    protected $creditProductIndex;
+
+    /**
+     * @param CreditProductIndex $creditProductIndex
+     */
+    public function __inject(CreditProductIndex $creditProductIndex)
+    {
+        $this->creditProductIndex = $creditProductIndex;
+    }
+    /**
+     * @param CreditProductIndex $creditProductIndex
+     * @param $button
+     */
+    public function test(CreditProductIndex $creditProductIndex, $button)
+    {
+        $this->creditProductIndex->open();
+        $this->creditProductIndex->getGridPageActions()->addNew();
+    }
+}
