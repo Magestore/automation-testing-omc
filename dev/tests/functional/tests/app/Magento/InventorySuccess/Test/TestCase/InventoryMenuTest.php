c68d64cf7d98adf09674b45054b79211df165a2d
Vinh Nguyen <viha195@gmail.com>
2017-11-22 15:08:32 +0700

    Inventory - Menu Test

---
+++ dev/tests/functional/tests/app/Magento/InventorySuccess/Test/TestCase/InventoryMenuTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinh
+ * Date: 22/11/2017
+ * Time: 14:14
+ */
+
+namespace Magento\InventorySuccess\Test\TestCase;
+
+
+use Magento\Backend\Test\TestCase\NavigateMenuTest;
+
+class InventoryMenuTest extends NavigateMenuTest
+{
+
+}
