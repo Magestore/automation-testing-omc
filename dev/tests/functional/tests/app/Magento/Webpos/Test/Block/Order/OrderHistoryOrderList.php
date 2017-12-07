0ecaa68b3c64d6fbeb038a129c246e6ab8aaf3ef
Thomas <thomas@trueplus.vn>
2017-12-07 13:35:42 +0700

    Update TestCase WebPOS. Cart Page Action Menu.

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Order/OrderHistoryOrderList.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 07/12/2017
+ * Time: 13:29
+ */
+
+namespace Magento\Webpos\Test\Block\Order;
+
+use Magento\Mtf\Block\Block;
+/**
+ * Class OrderHistoryOrderList
+ * @package Magento\Webpos\Test\Block\Order
+ */
+class OrderHistoryOrderList extends Block
+{
+
+}
---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Order/OrderHistoryOrderViewContent.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 07/12/2017
+ * Time: 13:30
+ */
+
+namespace Magento\Webpos\Test\Block\Order;
+
+use Magento\Mtf\Block\Block;
+/**
+ * Class OrderHistoryOrderViewContent
+ * @package Magento\Webpos\Test\Block\Order
+ */
+class OrderHistoryOrderViewContent extends Block
+{
+
+}
