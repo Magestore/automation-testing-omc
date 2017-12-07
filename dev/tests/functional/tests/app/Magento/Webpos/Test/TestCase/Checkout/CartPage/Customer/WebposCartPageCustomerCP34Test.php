3349b7e1d2caf4d17126df3f8f24cc74429fb9b3
Vinh Nguyen <viha195@gmail.com>
2017-12-07 13:48:12 +0700

    Webpos-Checkout-Cart Page-Customer-CP34 Part 1

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Checkout/CartPage/Customer/WebposCartPageCustomerCP34Test.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinh
+ * Date: 07/12/2017
+ * Time: 11:09
+ */
+
+namespace Magento\Webpos\Test\TestCase\Checkout\CartPage\Customer;
+
+
+use Magento\Catalog\Test\Fixture\CatalogProductSimple;
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Webpos\Test\Constraint\Checkout\CheckGUI\AssertWebposCheckoutPagePlaceOrderPageSuccessVisible;
+use Magento\Webpos\Test\Fixture\Staff;
+use Magento\Webpos\Test\Page\WebposIndex;
+
+class WebposCartPageCustomerCP34Test extends Injectable
+{
+	/**
+	 * @var WebposIndex
+	 */
+	protected $webposIndex;
+
+	/**
+	 * @var AssertWebposCheckoutPagePlaceOrderPageSuccessVisible
+	 */
+	protected $assertWebposCheckoutPagePlaceOrderPageSuccessVisible;
+
+	public function __inject(
+		WebposIndex $webposIndex,
+		AssertWebposCheckoutPagePlaceOrderPageSuccessVisible $assertWebposCheckoutPagePlaceOrderPageSuccessVisible
+	)
+	{
+		$this->webposIndex = $webposIndex;
+		$this->assertWebposCheckoutPagePlaceOrderPageSuccessVisible = $assertWebposCheckoutPagePlaceOrderPageSuccessVisible;
+	}
+
+	public function test(
+		Staff $staff,
+		CatalogProductSimple $product
+	)
+	{
+		$this->webposIndex->open();
+		if ($this->webposIndex->getLoginForm()->isVisible()) {
+			$this->webposIndex->getLoginForm()->fill($staff);
+			$this->webposIndex->getLoginForm()->clickLoginButton();
+			sleep(5);
+			while ($this->webposIndex->getFirstScreen()->isVisible()) {}
+			sleep(2);
+		}
+
+		$this->webposIndex->getCheckoutProductList()->waitProductListToLoad();
+
+		$this->webposIndex->getCheckoutProductList()->search($product->getName());
+		$this->webposIndex->getCheckoutProductList()->waitProductListToLoad();
+		$this->webposIndex->getMsWebpos()->waitCartLoader();
+
+		$this->webposIndex->getCheckoutCartFooter()->getButtonCheckout()->click();
+		$this->webposIndex->getMsWebpos()->waitCartLoader();
+		$this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+
+		$this->webposIndex->getCheckoutPaymentMethod()->getCashInMethod()->click();
+		$this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+		$this->webposIndex->getCheckoutPlaceOrder()->getButtonPlaceOrder()->click();
+		$this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+
+		//Assert Place Order Success
+		$this->assertWebposCheckoutPagePlaceOrderPageSuccessVisible->processAssert($this->webposIndex);
+
+		$this->webposIndex->getCheckoutSuccess()->getNewOrderButton()->click();
+		$this->webposIndex->getMsWebpos()->waitCartLoader();
+	}
+}
