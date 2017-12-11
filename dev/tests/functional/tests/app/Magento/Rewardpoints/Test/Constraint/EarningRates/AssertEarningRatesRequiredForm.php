36ed62f2fc9c569f46176a96e89262a7533ba93c
Puddin <puddin@trueplus.vn>
2017-12-11 14:20:47 +0700

    update Earning Rates Form Field Test

---
+++ dev/tests/functional/tests/app/Magento/Rewardpoints/Test/Constraint/EarningRates/AssertEarningRatesRequiredForm.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PhucDo
+ * Date: 12/11/2017
+ * Time: 1:29 PM
+ */
+
+namespace Magento\Rewardpoints\Test\Constraint\EarningRates;
+
+
+use Magento\Mtf\Constraint\AbstractConstraint;
+use Magento\Rewardpoints\Test\Page\Adminhtml\EarningRatesNew;
+
+
+class AssertEarningRatesRequiredForm extends AbstractConstraint
+{
+
+    /**
+     * @param EarningRatesNew $earningRatesNew
+     */
+    public function processAssert(EarningRatesNew $earningRatesNew)
+    {
+        \PHPUnit_Framework_Assert::assertTrue(
+            $earningRatesNew->getEarningRatesForm()->fieldErrorIsVisible(),
+            'EarningRates form is not required field visible.'
+        );
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'EarningRates form is available.';
+    }
+}
