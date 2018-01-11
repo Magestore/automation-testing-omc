26a897a1215108580b7becd4e7441ddd838dee88
bangtrueplus <bang@trueplus.vn>
2018-01-11 15:48:12 +0700

    change taxrate

--- dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Tax/WebposTaxTAX17Test.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Tax/WebposTaxTAX17Test.php
@@ -36,6 +36,9 @@ class WebposTaxTAX17Test extends Injectable
     {
         $customer = $fixtureFactory->createByCode('customer', ['dataset' => 'johndoe_MI_unique_first_name']);
         $customer->persist();
+        //change taxRate
+        $taxRate = $fixtureFactory->createByCode('taxRate', ['dataset'=> 'US-MI-Rate_1']);
+        $this->objectManager->create('Magento\Tax\Test\Handler\TaxRate\Curl')->persist($taxRate);
 
         return ['customer' => $customer];
     }
