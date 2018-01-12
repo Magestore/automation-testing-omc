4de9eb7793aac9791b406f742866c872bbdc67da
Puddin <puddin@trueplus.vn>
2018-01-12 16:00:55 +0700

    Upload TAX47, TAX49, TAX50

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Tax/WebposTaxTAX49Test.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PhucDo
+ * Date: 1/12/2018
+ * Time: 2:03 PM
+ */
+
+namespace Magento\Webpos\Test\TestCase\Tax;
+
+use Magento\Customer\Test\Fixture\Customer;
+use Magento\Mtf\Fixture\FixtureFactory;
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Webpos\Test\Constraint\Tax\AssertTaxAmountOnOrderPageWithShippingFee;
+use Magento\Webpos\Test\Page\WebposIndex;
+
+/**
+ * Class WebposTaxTAX49Test
+ * @package Magento\Webpos\Test\TestCase\Tax
+ */
+class WebposTaxTAX49Test extends Injectable
+{
+    /**
+     * @var WebposIndex
+     */
+    protected $webposIndex;
+
+    /**
+     * @var FixtureFactory
+     */
+    protected $fixtureFactory;
+
+    /**
+     * @var AssertTaxAmountOnOrderPageWithShippingFee
+     */
+    protected $assertTaxAmountOnOrderPageWithShippingMethod;
+
+    /**
+     * Prepare data.
+     *
+     * @param FixtureFactory $fixtureFactory
+     * @return array
+     */
+    public function __prepare(FixtureFactory $fixtureFactory)
+    {
+        // Config system value
+        $this->objectManager->getInstance()->create(
+            'Magento\Config\Test\TestStep\SetupConfigurationStep',
+            ['configData' => 'default_tax_configuration_use_system_value']
+        )->run();
+
+        // Change TaxRate
+        $taxRate = $fixtureFactory->createByCode('taxRate', ['dataset' => 'US-MI-Rate_1']);
+        $this->objectManager->create('Magento\Tax\Test\Handler\TaxRate\Curl')->persist($taxRate);
+
+        // Add Customer
+        $customer = $fixtureFactory->createByCode('customer', ['dataset' => 'customer_MI']);
+        $customer->persist();
+
+        return [
+            'customer' => $customer,
+            'taxRate' => $taxRate->getRate()
+        ];
+    }
+
+    /**
+     * @param WebposIndex $webposIndex
+     * @param FixtureFactory $fixtureFactory
+     * @param AssertTaxAmountOnOrderPageWithShippingFee $assertTaxAmountOnOrderPageWithShippingMethod
+     */
+    public function __inject(
+        WebposIndex $webposIndex,
+        FixtureFactory $fixtureFactory,
+        AssertTaxAmountOnOrderPageWithShippingFee $assertTaxAmountOnOrderPageWithShippingMethod
+    )
+    {
+        $this->webposIndex = $webposIndex;
+        $this->fixtureFactory = $fixtureFactory;
+        $this->assertTaxAmountOnOrderPageWithShippingMethod = $assertTaxAmountOnOrderPageWithShippingMethod;
+    }
+
+    /**
+     * @param Customer $customer
+     * @param $products
+     * @param $configData
+     * @param $taxRate
+     * @param bool $createInvoice
+     * @param bool $shipped
+     * @return array
+     */
+    public function test(
+        Customer $customer,
+        $products,
+        $configData,
+        $taxRate,
+        $createInvoice = true,
+        $shipped = false
+    )
+    {
+        // Create products
+        $products = $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\CreateNewProductsStep',
+            ['products' => $products]
+        )->run();
+
+        // Config
+        $this->objectManager->getInstance()->create(
+            'Magento\Config\Test\TestStep\SetupConfigurationStep',
+            ['configData' => $configData]
+        )->run();
+
+        // Login webpos
+        $staff = $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\LoginWebposStep'
+        )->run();
+
+        // Add product to cart
+        $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\AddProductToCartStep',
+            ['products' => $products]
+        )->run();
+
+        // Change customer in cart
+        $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\ChangeCustomerOnCartStep',
+            ['customer' => $customer]
+        )->run();
+
+        // Check out
+        $this->webposIndex->getCheckoutCartFooter()->getButtonCheckout()->click();
+        $this->webposIndex->getMsWebpos()->waitCartLoader();
+        $this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+
+        // Select Shipping Method
+        $this->webposIndex->getCheckoutShippingMethod()->openCheckoutShippingMethod();
+        $this->webposIndex->getCheckoutShippingMethod()->getFlatRateFixed()->click();
+        $this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+        $shippingFee = $this->webposIndex->getCheckoutShippingMethod()->getShippingMethodPrice("Flat Rate - Fixed")->getText();
+        $shippingFee = (float)substr($shippingFee,1);
+
+        // Select Payment Method
+        $this->webposIndex->getCheckoutPaymentMethod()->getCashInMethod()->click();
+        $this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+
+        $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\PlaceOrderSetShipAndCreateInvoiceSwitchStep',
+            [
+                'createInvoice' => $createInvoice,
+                'shipped' => $shipped
+            ]
+        )->run();
+
+        $this->webposIndex->getCheckoutPlaceOrder()->getButtonPlaceOrder()->click();
+        $this->webposIndex->getMsWebpos()->waitCheckoutLoader();
+        // End Place Order
+
+        $orderId = str_replace('#' , '', $this->webposIndex->getCheckoutSuccess()->getOrderId()->getText());
+
+        $this->webposIndex->getCheckoutSuccess()->getNewOrderButton()->click();
+        $this->webposIndex->getMsWebpos()->waitCartLoader();
+
+        $this->webposIndex->getMsWebpos()->clickCMenuButton();
+        $this->webposIndex->getCMenu()->ordersHistory();
+
+        sleep(2);
+        $this->webposIndex->getOrderHistoryOrderList()->waitLoader();
+
+        $this->webposIndex->getOrderHistoryOrderList()->getFirstOrder()->click();
+        while (strcmp($this->webposIndex->getOrderHistoryOrderViewHeader()->getStatus(), 'Not Sync') == 0) {}
+        self::assertEquals(
+            $orderId,
+            $this->webposIndex->getOrderHistoryOrderViewHeader()->getOrderId(),
+            "Order Content - Order Id is wrong"
+            . "\nExpected: " . $orderId
+            . "\nActual: " . $this->webposIndex->getOrderHistoryOrderViewHeader()->getOrderId()
+        );
+
+        //Assert Tax Amount on Orders History
+        $this->assertTaxAmountOnOrderPageWithShippingMethod->processAssert($taxRate, $shippingFee, $products, $this->webposIndex);
+        //End Assert Tax Amount on Orders History
+
+        return [
+            'products' => $products,
+            'taxRate' => $taxRate
+        ];
+    }
+
+    /**
+     *
+     */
+    public function tearDown()
+    {
+        // Config system value
+        $this->objectManager->getInstance()->create(
+            'Magento\Config\Test\TestStep\SetupConfigurationStep',
+            ['configData' => 'default_tax_configuration_use_system_value']
+        )->run();
+    }
+}
