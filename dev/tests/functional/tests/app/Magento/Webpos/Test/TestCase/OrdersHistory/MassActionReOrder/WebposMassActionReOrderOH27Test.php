89676018c2aa3a0c68630aef88c06b12dd084c23
Puddin <puddin@trueplus.vn>
2018-01-25 17:03:35 +0700

    Upload OH27 -> OH28

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/OrdersHistory/MassActionReOrder/WebposMassActionReOrderOH27Test.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PhucDo
+ * Date: 1/25/2018
+ * Time: 10:33 AM
+ */
+
+namespace Magento\Webpos\Test\TestCase\OrdersHistory\MassActionReOrder;
+
+use Magento\Sales\Test\Fixture\OrderInjectable;
+use Magento\Webpos\Test\Page\WebposIndex;
+use Magento\Mtf\TestCase\Injectable;
+
+class WebposMassActionReOrderOH27Test extends Injectable
+{
+    /**
+     * @var WebposIndex
+     */
+    protected $webposIndex;
+
+    public function __inject(WebposIndex $webposIndex)
+    {
+        $this->webposIndex = $webposIndex;
+    }
+
+    public function test(
+        OrderInjectable $order
+    )
+    {
+        // Preconditions
+        $order->persist();
+        $products = $order->getEntityId()['products'];
+//        foreach ($products as $product) {
+//            \Zend_Debug::dump($product->getName());
+//        }
+
+        // Login webpos
+        $staff = $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\LoginWebposStep'
+        )->run();
+
+        // Step
+        $this->webposIndex->getCheckoutProductList()->waitProductListToLoad();
+        $this->webposIndex->getMsWebpos()->clickCMenuButton();
+        $this->webposIndex->getCMenu()->ordersHistory();
+        $this->webposIndex->getOrderHistoryOrderList()->waitLoader();
+        $this->webposIndex->getOrderHistoryOrderList()->getFirstOrder()->click();
+
+        $this->webposIndex->getOrderHistoryOrderViewHeader()->openAddOrderNote();
+        $this->webposIndex->getOrderHistoryOrderViewHeader()->getAction('Re-order')->click();
+
+        $this->webposIndex->getCheckoutProductList()->waitProductListToLoad();
+
+        return [
+            'products' => $products
+        ];
+    }
+}
