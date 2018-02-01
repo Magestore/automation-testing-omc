68d0e849e282156a22f79c0b74dad481d0c53c34
bangtrueplus <bang@trueplus.vn>
2018-02-01 11:37:39 +0700

    OH6667

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Block/ManageStocks/ManageStockList.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vong
+ * Date: 2/1/2018
+ * Time: 9:10 AM
+ */
+
+namespace Magento\Webpos\Test\Block\ManageStocks;
+
+use Magento\Mtf\Block\Block;
+use Magento\Mtf\Client\Locator;
+
+class ManageStockList extends Block
+{
+    public function getSearchBox()
+    {
+        return $this->_rootElement->find('[id="search-header-stock"]');
+    }
+
+    public function searchProduct($value)
+    {
+        $this->getSearchBox()->setValue($value);
+    }
+
+    public function getProductRow($productName)
+    {
+        $rowTemplate = './/tr[.//span[@data-bind="text: name" and text()="%s"]]';
+        return $this->_rootElement->find(sprintf($rowTemplate, $productName), Locator::SELECTOR_XPATH);
+    }
+
+    public function getProductQtyInput($productName)
+    {
+        $row = $this->getProductRow($productName);
+        return $row->find('input.qty-edit');
+    }
+
+    public function getProductQtyValue($productName)
+    {
+        return $this->getProductQtyInput($productName)->getValue();
+    }
+}
