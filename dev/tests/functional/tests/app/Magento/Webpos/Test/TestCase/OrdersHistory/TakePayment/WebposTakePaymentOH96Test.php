2db6c2c76b71122da8220669140309d510eb9e07
WebS2B <ducvuk6196@gmail.com>
2018-02-06 08:03:52 +0700

    2.2.1 takepayment fix 92-99

--- dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/OrdersHistory/TakePayment/WebposTakePaymentOH96Test.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/OrdersHistory/TakePayment/WebposTakePaymentOH96Test.php
@@ -13,7 +13,7 @@ use Magento\Catalog\Test\Fixture\CatalogProductSimple;
 use Magento\Mtf\Fixture\FixtureFactory;
 use Magento\Webpos\Test\Constraint\Checkout\CheckGUI\AssertWebposCheckoutPagePlaceOrderPageSuccessVisible;
 
-class WebposTakePaymentOH92Test extends Injectable
+class WebposTakePaymentOH96Test extends Injectable
 {
     /**
      * @var WebposIndex $webposIndex
@@ -105,11 +105,10 @@ class WebposTakePaymentOH92Test extends Injectable
         //click take payment
         $this->webposIndex->getOrderHistoryOrderViewHeader()->getTakePaymentButton()->click();
         $this->webposIndex->getOrderHistoryPayment()->getPaymentMethod("Web POS - Cash In")->click();
-
-        $this->webposIndex->getOrderHistoryPayment()->getInputAmount()->setValue($amount);
+        sleep(1);
+        $am = $this->webposIndex->getOrderHistoryOrderViewHeader()->getGrandTotal();
+        $this->webposIndex->getOrderHistoryPayment()->getInputAmount()->setValue(substr($am,1));
         $this->webposIndex->getMsWebpos()->clickOutsidePopup();
-
-        $this->webposIndex->getOrderHistoryPayment()->getAddMorePaymentButton()->click();
     }
 
     public function tearDown()
