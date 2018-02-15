3efef8741ac6005541aa6a1b7900605621068e45
Puddin <puddin@trueplus.vn>
2018-02-15 21:02:15 +0700

    Upload TestCase PG29 -> 31

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/ProductsGrid/BundleProduct/WebposProductsGridPG30Test.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PhucDo
+ * Date: 2/12/2018
+ * Time: 10:49 AM
+ */
+
+namespace Magento\Webpos\Test\TestCase\ProductsGrid\BundleProduct;
+
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Webpos\Test\Page\WebposIndex;
+use Magento\Webpos\Test\Constraint\ProductsGrid\SimpleProduct\AssertProductQtyInProductList;
+
+/**
+ * Class WebposProductsGridPG30Test
+ * @package Magento\Webpos\Test\TestCase\ProductsGrid\BundleProduct
+ */
+class WebposProductsGridPG30Test extends Injectable
+{
+    /**
+     * @var WebposIndex
+     */
+    protected $webposIndex;
+
+    /**
+     * @param WebposIndex $webposIndex
+     */
+    public function __inject(
+        WebposIndex $webposIndex
+    )
+    {
+        $this->webposIndex = $webposIndex;
+    }
+
+    /**
+     * @param $products
+     * @return array
+     */
+    public function test(
+        $products
+    )
+    {
+        // Create products
+        $products = $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\CreateNewProductsStep',
+            ['products' => $products]
+        )->run();
+
+        // Login webpos
+        $staff = $this->objectManager->getInstance()->create(
+            'Magento\Webpos\Test\TestStep\LoginWebposStep'
+        )->run();
+
+        $this->webposIndex->getCheckoutProductList()->waitProductListToLoad();
+        $this->webposIndex->getCheckoutProductList()->search($products[0]['product']->getSku());
+        $this->webposIndex->getCheckoutProductList()->waitProductListToLoad();
+
+        return [
+            'products' => $products
+        ];
+    }
+}
