5d494adf1fbec13133aa87c4dd233d135c418af1
Vinh Nguyen <viha195@gmail.com>
2018-02-23 13:34:33 +0700

    Webpos - Customer On Checkout Page - Shipping Address popup - CC13->CC17

--- dev/tests/functional/tests/app/Magento/Webpos/Test/Constraint/CustomerOnCheckoutPage/ShippingAddressPopup/AssertCreatedAddressIsShownOnNewCustomerPopup.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Constraint/CustomerOnCheckoutPage/ShippingAddressPopup/AssertCreatedAddressIsShownOnNewCustomerPopup.php
@@ -13,30 +13,43 @@ use Magento\Customer\Test\Fixture\Address;
 use Magento\Mtf\Constraint\AbstractConstraint;
 use Magento\Webpos\Test\Page\WebposIndex;
 
+/**
+ * Class AssertCreatedAddressIsShownOnNewCustomerPopup
+ * @package Magento\Webpos\Test\Constraint\CustomerOnCheckoutPage\ShippingAddressPopup
+ */
 class AssertCreatedAddressIsShownOnNewCustomerPopup extends AbstractConstraint
 {
-	public function processAssert(WebposIndex $webposIndex, Address $address)
+	/**
+	 * @param WebposIndex $webposIndex
+	 * @param Address $address
+	 */
+	public function processAssert(
+		WebposIndex $webposIndex,
+		Address $address
+	)
 	{
 		$country= [
 			'United States' => 'US',
 			'United Kingdom' => 'GB',
 			'Germany' => 'DE'
 		];
-		$address = $address->getFirstname().' '.$address->getLastname().', '
+		$addressText = $address->getFirstname().' '.$address->getLastname().', '
 			.$address->getStreet().' '.$address->getCity().', '
 			.$country[$address->getCountryId()].', '
 			.$address->getPostcode().', '
 			.$address->getTelephone();
-		\PHPUnit_Framework_Assert::assertEquals(
-			$address,
-			$webposIndex->getCheckoutAddCustomer()->getShippingAddressBox()->getText(),
-			"Customer on checkout page - Shipping address popup - Shipping address is not shown correct on new customer popup"
-		);
-		\PHPUnit_Framework_Assert::assertEquals(
-			$address,
-			$webposIndex->getCheckoutAddCustomer()->getBillingAddressBox()->getText(),
-			"Customer on checkout page - Shipping address popup - Billing address is not shown correct on new customer popup"
-		);
+//		\PHPUnit_Framework_Assert::assertEquals(
+//			$address,
+//			$webposIndex->getCheckoutAddCustomer()->getShippingAddressBox()->getText(),
+//			"Customer on checkout page - Shipping address popup - Shipping address is not shown correct on new customer popup"
+//		);
+//		\PHPUnit_Framework_Assert::assertEquals(
+//			$address,
+//			$webposIndex->getCheckoutAddCustomer()->getBillingAddressBox()->getText(),
+//			"Customer on checkout page - Shipping address popup - Billing address is not shown correct on new customer popup"
+//		);
+		$this->objectManager->create(AssertShippingAddressOnNewCustomerPopupIsCorrect::class)->processAssert($webposIndex, $addressText);
+		$this->objectManager->create(AssertBillingAddressOnNewCustomerPopupIsCorrect::class)->processAssert($webposIndex, $addressText);
 	}
 
 	/**
