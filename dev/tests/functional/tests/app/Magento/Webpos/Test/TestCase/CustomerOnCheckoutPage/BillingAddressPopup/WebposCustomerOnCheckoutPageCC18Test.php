09017f6e2e0ee88e42048c7afd51086837584768
Puddin <puddin@trueplus.vn>
2018-02-23 13:37:10 +0700

    Upload block checkout billing address

---
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/CustomerOnCheckoutPage/BillingAddressPopup/WebposCustomerOnCheckoutPageCC18Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PhucDo
+ * Date: 2/23/2018
+ * Time: 1:24 PM
+ */
+
+namespace Magento\Webpos\Test\TestCase\CustomerOnCheckoutPage\BillingAddressPopup;
+
+
+class WebposCustomerOnCheckoutPageCC18Test
+{
+
+}
