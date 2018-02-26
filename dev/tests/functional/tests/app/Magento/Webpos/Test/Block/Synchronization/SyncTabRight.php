160813f9de27f6ca345ffb52807d541b130069e0
WebS2B <ducvuk6196@gmail.com>
2018-02-26 09:36:17 +0700

    update block sync vs contraint

--- dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Synchronization/SyncTabRight.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Synchronization/SyncTabRight.php
@@ -9,8 +9,22 @@
 namespace Magento\Webpos\Test\Block\Synchronization;
 
 use Magento\Mtf\Block\Block;
+use Magento\Mtf\Client\Locator;
 
 class SyncTabRight extends Block
 {
+    public function tabErrorLogs()
+    {
+        $this->_rootElement->find('//*[@id="block-webpos-settings-synchronization"]/div/div[1]/div/div/ul/li[2]/a', Locator::SELECTOR_XPATH);
+    }
 
+    public function tabSyncData()
+    {
+        $this->_rootElement->find('//*[@id="block-webpos-settings-synchronization"]/div/div[1]/div/div/ul/li[1]/a', Locator::SELECTOR_XPATH);
+    }
+
+    public function buttonResetLocal()
+    {
+        $this->_rootElement->find('.btn-reset-db');
+    }
 }
