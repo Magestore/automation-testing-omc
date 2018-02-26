ac389b5f8fe3bcc91f90d69fca9e48d5b7afb6dd
Vinh Nguyen <viha195@gmail.com>
2018-02-26 11:18:00 +0700

    Webpos - Sync - update Tab data block

--- dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Synchronization/SyncTabData.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Synchronization/SyncTabData.php
@@ -9,8 +9,44 @@
 namespace Magento\Webpos\Test\Block\Synchronization;
 
 use Magento\Mtf\Block\Block;
+use Magento\Mtf\Client\Locator;
 
 class SyncTabData extends Block
 {
+	public function getUpdateAllButton()
+	{
+		return $this->_rootElement->find('.update-all');
+	}
 
+	public function getReloadAllButton()
+	{
+		return $this->_rootElement->find('.reload-all');
+	}
+
+	// Get Item Row Elements
+	public function getItemRow($name)
+	{
+		return $this->_rootElement->find('//*[@id="sync_container"]/div/main/table/tbody/tr/td[text()="'.$name.'"]/..', Locator::SELECTOR_XPATH);
+	}
+
+	public function getItemRowUpdateButton($name)
+	{
+		return $this->getItemRow($name)->find(' td.process-box > div > button');
+	}
+
+	public function getItemRowReloadButton($name)
+	{
+		return $this->getItemRow($name)->find('td:nth-child(4) > button');
+	}
+
+	public function getItemRowProgress($name)
+	{
+		return $this->getItemRow($name)->find('.myProgress');
+	}
+
+	public function getItemRowSuccess($name)
+	{
+		return $this->getItemRow($name)->find('.icon-iconPOS-success');
+	}
+	//////////////////////////////
 }
