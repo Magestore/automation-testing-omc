5ce7498c35d79563f3acc80bfd567508df596c33
Puddin <puddin@trueplus.vn>
2018-03-08 08:47:26 +0700

    Upload Test ML 48 -> 51

--- dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Adminhtml/MappingLocation/MappingLocationGrid.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/Block/Adminhtml/MappingLocation/MappingLocationGrid.php
@@ -29,7 +29,7 @@ class MappingLocationGrid extends DataGrid
      *
      * @var string
      */
-    protected $spinner = '[data-component="os_webpos_locations_listing.os_webpos_locations_listing.os_webpos_locations_listing_columns"]';
+    protected $spinner = '[data-component="os_warehouse_locations_mapping_form.os_warehouse_locations_mapping_form"]';
 
     /**
      * Press 'Choose Locations' button.
