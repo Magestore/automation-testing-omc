d20b36de426316113f07ca4a6f855f212e8580a9
NgoVanHuy <ngovanhuy.cntt2@gmail.com>
2018-05-03 10:34:59 +0700

    Manage staff sort 2

--- dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Staff/CheckSortColumnInGrid/ManageStaffSortByColumnTest.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Staff/CheckSortColumnInGrid/ManageStaffSortByColumnTest.php
@@ -28,4 +28,35 @@ use Magento\Ui\Test\TestCase\GridSortingTest;
  */
 class ManageStaffSortByColumnTest extends GridSortingTest
 {
+    /**
+     * (non-PHPdoc)
+     * @see \Magento\Ui\Test\TestCase\GridSortingTest::test()
+     */
+    public function test(
+        $pageClass,
+        $gridRetriever,
+        array $columnsForSorting,
+        $fixtureName = null,
+        $fixtureDataSet = null,
+        $itemsCount = null,
+        array $steps = [],
+        $fixtureAddedDataSet = null
+    ) {
+        // Fill grid before sorting if needed
+        if ($fixtureName && $fixtureDataSet && $itemsCount && $steps) {
+            $this->createItems($itemsCount, $fixtureName, $fixtureDataSet, $steps);
+//            if ($fixtureAddedDataSet) {
+//                $this->createItems($itemsCount, $fixtureName, $fixtureAddedDataSet, $steps);
+//            }
+        }
+        $result = parent::test($pageClass, $gridRetriever, $columnsForSorting);
+
+        // Reset Filter to ID
+        $grid = $this->pageFactory->create($pageClass)
+            ->$gridRetriever()
+            ->sortGridByField('ID');
+        sleep(3); // Wait to save sort
+
+        return $result;
+    }
 }
