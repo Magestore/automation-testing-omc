ee97d1c24a35a08f1f6b6008486c46e2804a67d3
NgoVanHuy <ngovanhuy.cntt2@gmail.com>
2018-05-08 13:54:08 +0700

    manage staff sort by column

--- dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Staff/CheckSortColumnInGrid/ManageStaffSortByColumnTest.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Staff/CheckSortColumnInGrid/ManageStaffSortByColumnTest.php
@@ -9,6 +9,9 @@
 namespace Magento\Webpos\Test\TestCase\Staff\CheckSortColumnInGrid;
 
 use Magento\Ui\Test\TestCase\GridSortingTest;
+use Magento\Webpos\Test\Fixture\Location;
+use Magento\Webpos\Test\Fixture\Staff;
+use Magento\Webpos\Test\Fixture\Pos;
 
 /**
  * Class ManageStaffSortByColumnTest\
@@ -40,22 +43,46 @@ class ManageStaffSortByColumnTest extends GridSortingTest
         $fixtureDataSet = null,
         $itemsCount = null,
         array $steps = [],
+        $fixtureAddedName = null,
         $fixtureAddedDataSet = null
     ) {
-        // Fill grid before sorting if needed
-        if ($fixtureName && $fixtureDataSet && $itemsCount && $steps) {
-            $this->createItems($itemsCount, $fixtureName, $fixtureDataSet, $steps);
-//            if ($fixtureAddedDataSet) {
-//                $this->createItems($itemsCount, $fixtureName, $fixtureAddedDataSet, $steps);
-//            }
+        if($fixtureName && $fixtureDataSet){
+            //Create role and staff for role
+            /**@var Location $location */
+            $location = $this->fixtureFactory->createByCode('location', ['dataset' => 'default']);
+            $location->persist();
+            $locationId = $location->getLocationId();
+            $posData['pos_name'] = 'Pos Test %isolation%';
+            $posData['status'] = 'Enabled';
+            $array = [];
+            $array[] = $locationId;
+            $posData['location_id'] = $array;
+            /**@var Pos $pos */
+            $pos = $this->fixtureFactory->createByCode('pos', ['data' => $posData]);
+            $pos->persist();
+            $posId = $pos->getPosId();
+            $staff = $this->fixtureFactory->createByCode('staff', ['dataset' => 'staffMS03']);
+            $staffData = $staff->getData();
+            $array = [];
+            $array[] = $locationId;
+            $staffData['location_id'] = $array;
+            $array = [];
+            $array[] = $posId;
+            $staffData['pos_ids'] = $array;
+            /**@var Staff $staff */
+            $staff = $this->fixtureFactory->createByCode('staff', ['data' => $staffData]);
+            $staff->persist();
+            $webposRole = $this->fixtureFactory->createByCode('webposRole', ['dataset' => 'role_ms77']);
+            $roleData = $webposRole->getData();
+            $array  = [];
+            $array[] = $staff->getStaffId();
+            $roleData['staff_id'] = $array;
+            $role = $this->fixtureFactory->createByCode('webposRole', ['data' => $roleData]);
+            $role->persist();
         }
-        $result = parent::test($pageClass, $gridRetriever, $columnsForSorting);
 
-        // Reset Filter to ID
-        $grid = $this->pageFactory->create($pageClass)
-            ->$gridRetriever()
-            ->sortGridByField('ID');
-        sleep(3); // Wait to save sort
+        $result = parent::test($pageClass, $gridRetriever, $columnsForSorting,
+            null, null, null, $steps);
 
         return $result;
     }
