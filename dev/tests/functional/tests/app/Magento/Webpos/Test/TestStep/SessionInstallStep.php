a579ec470e934427f2a4f6fca1d291ca181acad9
rooney <hungnt167@gmail.com>
2018-05-10 09:37:10 +0700

    fix bug Webpos product grid 23, 26 , 56

--- dev/tests/functional/tests/app/Magento/Webpos/Test/TestStep/SessionInstallStep.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestStep/SessionInstallStep.php
@@ -7,27 +7,95 @@
  */
 namespace  Magento\Webpos\Test\TestStep;
 
+use Magento\Mtf\Fixture\FixtureFactory;
+use Magento\Webpos\Test\Page\WebposIndex;
+use Magento\Mtf\Config\DataInterface;
 
 /**
  * Class LoginWebposStep
  * @package Magento\Webpos\Test\TestStep
  */
-class SessionInstallStep extends LoginWebposStep
+class SessionInstallStep
 {
+    /**
+     * System config.
+     *
+     * @var DataInterface
+     */
+    protected $configuration;
+
+    /**
+     * Webpos Index page.
+     * @var WebposIndex
+     */
+    protected $webposIndex;
 
 
-	public function run()
-	{
+    /**
+     * @var FixtureFactory
+     */
+    protected $fixtureFactory;
+
+    /**
+     * @param WebposIndex $webposIndex
+     */
+    public function __construct(
+        WebposIndex $webposIndex,
+        DataInterface $configuration,
+        FixtureFactory $fixtureFactory
+    ) {
+        $this->webposIndex = $webposIndex;
+        $this->configuration = $configuration;
+        $this->fixtureFactory = $fixtureFactory;
+    }
+
+    public function run()
+    {
         \Magento\Mtf\ObjectManager::getInstance()->create(
             'Magento\Config\Test\TestStep\SetupConfigurationStep',
             ['configData' => 'create_section_before_working_no_MS57']
         )->run();
-		$staff = parent::run();
+
+        $username = $this->configuration->get('application/0/backendLogin/0/value');
+        $password = $this->configuration->get('application/0/backendPassword/0/value');
+
+
+        $this->webposIndex->open();
+        if ($this->webposIndex->getLoginForm()->isVisible()) {
+            $this->webposIndex->getLoginForm()->getUsernameField()->setValue($username);
+            $this->webposIndex->getLoginForm()->getPasswordField()->setValue($password);
+            $this->webposIndex->getLoginForm()->clickLoginButton();
+            sleep(1);
+            $time = time();
+            $timeAfter = $time + 30;
+            while (!$this->webposIndex->getWrapWarningForm()->isVisible() &&
+                !$this->webposIndex->getWrapWarningForm()->getButtonContinue()->isVisible() &&
+                $time < $timeAfter){
+                $time = time();
+            }
+            if ($this->webposIndex->getWrapWarningForm()->isVisible() &&
+                $this->webposIndex->getWrapWarningForm()->getButtonContinue()->isVisible()) {
+                $this->webposIndex->getWrapWarningForm()->getButtonContinue()->click();
+                sleep(1);
+            }
+        }
+
+        $data = [
+            'username' => $username,
+            'password' => $password
+        ];
+
         /**
          *  wait sync complete
          */
-        $this->webposIndex->getBody()->waitForElementNotVisible('.wrap-login-form');
-        sleep(3);
-        return $staff;
+        while (
+            ( rtrim($this->webposIndex->getSessionInstall()->getPercent()->getText(),"%") * 1 ) < 95
+        ) {
+            sleep(1);
+        }
+
+        sleep(5);
+        return $this->fixtureFactory->createByCode('staff' , ['data' => $data]);
     }
+
 }
