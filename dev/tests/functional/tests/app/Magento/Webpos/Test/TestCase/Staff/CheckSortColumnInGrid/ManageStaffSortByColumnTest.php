a02bd360be837ffe24d22b85951d4c95c6abc748
NgoVanHuy <ngovanhuy.cntt2@gmail.com>
2018-06-08 13:17:23 +0700

    ManageStaffSortByColumnTest create failure to view display name staff admin 2

--- dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Staff/CheckSortColumnInGrid/ManageStaffSortByColumnTest.php
+++ dev/tests/functional/tests/app/Magento/Webpos/Test/TestCase/Staff/CheckSortColumnInGrid/ManageStaffSortByColumnTest.php
@@ -44,42 +44,42 @@ class ManageStaffSortByColumnTest extends GridSortingTest
         $testCaseId = null
     )
     {
-        if ($testCaseId && $testCaseId === "MS03") {
-            if ($fixtureName && $fixtureDataSet) {
-                //Create role and staff for role
-                /**@var Location $location */
-                $location = $this->fixtureFactory->createByCode('location', ['dataset' => 'default']);
-                $location->persist();
-                $locationId = $location->getLocationId();
-                $posData['pos_name'] = 'Pos Test %isolation%';
-                $posData['status'] = 'Enabled';
-                $array = [];
-                $array[] = $locationId;
-                $posData['location_id'] = $array;
-                /**@var Pos $pos */
-                $pos = $this->fixtureFactory->createByCode('pos', ['data' => $posData]);
-                $pos->persist();
-                $posId = $pos->getPosId();
-                $staff = $this->fixtureFactory->createByCode('staff', ['dataset' => 'staffMS03']);
-                $staffData = $staff->getData();
-                $array = [];
-                $array[] = $locationId;
-                $staffData['location_id'] = $array;
-                $array = [];
-                $array[] = $posId;
-                $staffData['pos_ids'] = $array;
-                /**@var Staff $staff */
-                $staff = $this->fixtureFactory->createByCode('staff', ['data' => $staffData]);
-                $staff->persist();
-                $webposRole = $this->fixtureFactory->createByCode('webposRole', ['dataset' => 'role_ms77']);
-                $roleData = $webposRole->getData();
-                $array = [];
-                $array[] = $staff->getStaffId();
-                $roleData['staff_id'] = $array;
-                $role = $this->fixtureFactory->createByCode('webposRole', ['data' => $roleData]);
-                $role->persist();
-            }
-        }
+//        if ($testCaseId && $testCaseId === "MS03") {
+//            if ($fixtureName && $fixtureDataSet) {
+//                //Create role and staff for role
+//                /**@var Location $location */
+//                $location = $this->fixtureFactory->createByCode('location', ['dataset' => 'default']);
+//                $location->persist();
+//                $locationId = $location->getLocationId();
+//                $posData['pos_name'] = 'Pos Test %isolation%';
+//                $posData['status'] = 'Enabled';
+//                $array = [];
+//                $array[] = $locationId;
+//                $posData['location_id'] = $array;
+//                /**@var Pos $pos */
+//                $pos = $this->fixtureFactory->createByCode('pos', ['data' => $posData]);
+//                $pos->persist();
+//                $posId = $pos->getPosId();
+//                $staff = $this->fixtureFactory->createByCode('staff', ['dataset' => 'staffMS03']);
+//                $staffData = $staff->getData();
+//                $array = [];
+//                $array[] = $locationId;
+//                $staffData['location_id'] = $array;
+//                $array = [];
+//                $array[] = $posId;
+//                $staffData['pos_ids'] = $array;
+//                /**@var Staff $staff */
+//                $staff = $this->fixtureFactory->createByCode('staff', ['data' => $staffData]);
+//                $staff->persist();
+//                $webposRole = $this->fixtureFactory->createByCode('webposRole', ['dataset' => 'role_ms77']);
+//                $roleData = $webposRole->getData();
+//                $array = [];
+//                $array[] = $staff->getStaffId();
+//                $roleData['staff_id'] = $array;
+//                $role = $this->fixtureFactory->createByCode('webposRole', ['data' => $roleData]);
+//                $role->persist();
+//            }
+//        }
         $result = parent::test($pageClass, $gridRetriever, $columnsForSorting,
             null, null, null, $steps);
         $this->assertTrue(false);
